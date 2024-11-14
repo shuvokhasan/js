d68609aa7efe6fd6484ab5f883464600a9743f60
Kamrul Hasan Shuvo <57871878+shuvokhasan@users.noreply.github.com>
2024-11-14 23:05:42 +0600

    Add files via upload

---
+++ 3-checkEvenOdd.js
@@ -0,0 +1,12 @@
+// Checks even or odd number
+
+function checkEvenOdd(num) {
+    if ((num % 2) == 0) {
+        console.log("Even");
+    } else {
+        console.log("Odd");
+    }
+}
+
+checkEvenOdd(4);
+checkEvenOdd(7);
